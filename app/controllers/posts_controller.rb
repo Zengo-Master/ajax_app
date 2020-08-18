df4a3d49a89ff32ae7017d299100f48644846e62
Zengo-Master <8435740@gmail.com>
2020-08-18 11:31:15 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,12 @@
 class PostsController < ApplicationController
-  def index # アクションはindex
-    @posts = Post.all  # インスタンス変数の生成　全レコードを@postsに代入
-  end
 
-  def new # ページを表示するだけなので定義のみ
+  def index
+    @posts = Post.all.order(id: "DESC")
   end
 
   def create
     Post.create(content: params[:content]) # paramsに入ったcontentというデータを生成
-
-    # 以下でも保存される
-    # post = Post.new(content: params[:content])
-    # post.save
+    redirect_to action: :index # トップページにリダイレクト
   end
 
 end
