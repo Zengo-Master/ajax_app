947d3773927fc1c26d7e0638f4ef88279d304cf9
Zengo-Master <8435740@gmail.com>
2020-08-19 15:12:21 +0900

    Basic認証を導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
   def index  # indexアクションを定義した
     @post = "これはコントローラーで定義したインスタンス変数を確認するための文字列です"
+  end
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+     username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
+    end
   end
 end
