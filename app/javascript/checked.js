2052b70eb21a1d4d175515031d9c27493ded6f00
Zengo-Master <8435740@gmail.com>
2021-02-11 12:28:57 +0900

    Update checked.js

--- app/javascript/checked.js
+++ app/javascript/checked.js
@@ -1,15 +1,15 @@
 function check() {
-  const posts = document.getElementsByClassName("post"); // クラス
+  const posts = document.getElementsByClassName("post"); // 全クラス取得
   postsA = Array.from(posts); // 配列を生成
 
-  postsA.forEach(function (post) {　 // forEachメソッドが可能
+  postsA.forEach(function (post) {  // forEachメソッドが可能
     if (post.getAttribute("data-load") != null) {
       return null;
     }
     post.setAttribute("data-load", "true");
     post.addEventListener("click", (e) => {
-      const postId = post.getAttribute("data-id"); //　メモのidを取得
-      const XHR = new XMLHttpRequest(); //　オブジェクトを生成
+      const postId = post.getAttribute("data-id"); // メモのidを取得
+      const XHR = new XMLHttpRequest(); // オブジェクトを生成
       XHR.open("GET", `/posts/${postId}`, true);
       XHR.responseType = "json";
       XHR.send(); // ここで初めてリクエストが行える
@@ -30,7 +30,7 @@ function check() {
         alert("Request failed");
       };
 
-      e.preventDefault(); //　イベントハンドラーの実行後に今回のイベントをキャンセル
+      e.preventDefault(); // イベントハンドラーの実行後に今回のイベントをキャンセル
 
      });
    });
